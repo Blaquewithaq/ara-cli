b6278c31ddbc1a6e4a53b542a0e8dadb57c635ab
Blaquewithaq <blaquewithaq@gmail.com>
2023-12-09 21:45:31 -0600

    feat: added eslint, commander, test dir, cleaned up

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import {Command} from "commander";
+
+const program = new Command();
+
+program.
+    name("ara").
+    description("A multi-tool for bun based projects").
+    version(
+        "0.0.1",
+        "-v, --version",
+        "Output the current version"
+    );
+
+program.parse(process.argv);
---
+++ tests/e2e/index.spec.ts
@@ -0,0 +1,17 @@
+// import {describe, expect, test} from "bun:test";
+
+
+// describe(
+//     "ara",
+//     () => {
+
+//         test(
+//             "--help",
+//             async () => {
+//                 // const command = await $("pkg/bin/anomie --help");
+//                 // expect(command).toContain("Usage: ara [options] [command]");
+//             }
+//         );
+
+//     }
+// );
